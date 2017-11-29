24c847e72d693191e494af6f6ed2a7a4ffe39015
Parix <parham.toupaebrahimi@gmail.com>
2017-11-29 21:12:26 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './pomodoro.png';
+import './App.css';
+
+import {Form , FormControl , Button} from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">This would be a pomodoro timer</h1>
+        </header>
+
+        <Form inline>
+                  <FormControl
+                    className="time-input"
+                    placeholder='How much time do you need to focus?'
+                    onChange={event => this.setState({newDeadline: event.target.value})}
+                  />
+                  <Button onClick={() => this.changeDeadline()}>
+                    Submit
+                  </Button>
+        </Form>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
